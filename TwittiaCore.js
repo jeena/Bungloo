cc46e8852249a245b95f1bcdce775c8169c097f2
Samuel Reis <git@samuelreis.de>
2010-09-18 12:29:13 +0200

    fix url replace to include brackets

--- TwittiaCore.js
+++ TwittiaCore.js
@@ -306,7 +306,7 @@ Twittia.prototype.authorizationHeader = function(method, url, params) {
 }
 
 function replaceURLWithHTMLLinks(text) {
-	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_()|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 	return text.replace(exp,"<a href='$1'>$1</a>"); 
 }
 
