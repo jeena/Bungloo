f7240ea2e5e8489f8383bff763f2c5969813a459
noformnocontent <i@noformnocontent.com>
2013-02-13 00:15:12 +0100

    spaces to tabs for /Mac/ and /Linux/

--- Mac/ViewDelegate.m
+++ Mac/ViewDelegate.m
@@ -17,14 +17,14 @@
 
 	if (![message isKindOfClass:[NSDictionary class]]) return;
 	
-    NSString *viewName = @"TimelineView";
-    if (sender == mentionsView) viewName = @"MentionsView";
-    if (sender == conversationView) viewName = @"ConversationView";
-    if (sender == oauthView) viewName = @"OauthView";
-    if (sender == profileView) viewName = @"ProfileView";
-    
+	NSString *viewName = @"TimelineView";
+	if (sender == mentionsView) viewName = @"MentionsView";
+	if (sender == conversationView) viewName = @"ConversationView";
+	if (sender == oauthView) viewName = @"OauthView";
+	if (sender == profileView) viewName = @"ProfileView";
+	
 	NSLog(@"js<%@>: %@:%@: %@",
-        viewName,
+		viewName,
 		[[message objectForKey:@"sourceURL"] lastPathComponent],
 		[message objectForKey:@"lineNumber"],
 		[message objectForKey:@"message"]
@@ -32,94 +32,94 @@
 }
 
 - (void)webView:(WebView *)sender runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WebFrame *)frame {
-    NSString *viewName = @"TimelineView";
-    if (sender == mentionsView) viewName = @"MentionsView";
-    if (sender == conversationView) viewName = @"ConversationView";
-    if (sender == oauthView) viewName = @"OauthView";
+	NSString *viewName = @"TimelineView";
+	if (sender == mentionsView) viewName = @"MentionsView";
+	if (sender == conversationView) viewName = @"ConversationView";
+	if (sender == oauthView) viewName = @"OauthView";
 
 	NSLog(@"jsa<%@>: %@", viewName, message);
 }
 
 - (BOOL)webView:(WebView *)sender runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WebFrame *)frame {
-    NSInteger result = NSRunCriticalAlertPanel(NSLocalizedString(@"Bungloo", @""),   // title
-                                        message,                                    // message
-                                        NSLocalizedString(@"OK", @""),              // default button
-                                        NSLocalizedString(@"Cancel", @""),          // alt button
-                                        nil);
-    return NSAlertDefaultReturn == result;  
-    return NO;
+	NSInteger result = NSRunCriticalAlertPanel(NSLocalizedString(@"Bungloo", @""),   // title
+											   message,								 // message
+											   NSLocalizedString(@"OK", @""),		 // default button
+											   NSLocalizedString(@"Cancel", @""),	 // alt button
+											   nil);
+	return NSAlertDefaultReturn == result;  
+	return NO;
 }
 
 - (void)webView:(WebView *)sender decidePolicyForNavigationAction:(NSDictionary *)actionInformation request:(NSURLRequest *)request frame:(WebFrame *)frame decisionListener:(id <WebPolicyDecisionListener>)listener {
 	[listener ignore];
-    [[NSWorkspace sharedWorkspace] openURL:[request URL]];
+	[[NSWorkspace sharedWorkspace] openURL:[request URL]];
 }
 
 - (void)webView:(WebView *)sender didFinishLoadForFrame:(WebFrame *)frame {
-    
-    NSFileManager *fileManager = [NSFileManager defaultManager];
+	
+	NSFileManager *fileManager = [NSFileManager defaultManager];
 	NSString *pathToJsPlugin = [@"~/Library/Application Support/bungloo/Plugin.js" stringByExpandingTildeInPath];
 	NSString *pathToCssPlugin = [@"~/Library/Application Support/bungloo/Plugin.css" stringByExpandingTildeInPath];
 	
-    if([fileManager fileExistsAtPath:pathToCssPlugin])
-    {
-        [sender stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"setTimeout(function() { loadCssPlugin('file://localhost%@') }, 1000);", pathToCssPlugin]];
+	if([fileManager fileExistsAtPath:pathToCssPlugin])
+	{
+		[sender stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"setTimeout(function() { loadCssPlugin('file://localhost%@') }, 1000);", pathToCssPlugin]];
 	}
-    
-    if([fileManager fileExistsAtPath:pathToJsPlugin])
-    {
-        [sender stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"setTimeout(function() { loadJsPlugin('file://localhost%@') }, 1000);", pathToJsPlugin]];
+	
+	if([fileManager fileExistsAtPath:pathToJsPlugin])
+	{
+		[sender stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"setTimeout(function() { loadJsPlugin('file://localhost%@') }, 1000);", pathToJsPlugin]];
 	}
-    
-    [sender stringByEvaluatingJavaScriptFromString:@"var OS_TYPE = 'mac';"];
+	
+	[sender stringByEvaluatingJavaScriptFromString:@"var OS_TYPE = 'mac';"];
 
-    if (sender == oauthView) {
-        
-        [oauthView stringByEvaluatingJavaScriptFromString:@"function HostAppGo() { start('oauth') }"];
+	if (sender == oauthView) {
+		
+		[oauthView stringByEvaluatingJavaScriptFromString:@"function HostAppGo() { start('oauth') }"];
 
-    } else if(sender == conversationView) {
-    
-        [conversationView stringByEvaluatingJavaScriptFromString:@"function HostAppGo() { start('conversation') }"];
-        
-    } else if(sender == profileView) {
-        
-        [profileView stringByEvaluatingJavaScriptFromString:@"function HostAppGo() { start('profile') }"];
-        
-    } else {
-        
-        NSString *action = @"timeline";
-        NSString *delay = @"1";
-        
-        if (sender == mentionsView) {
-            action = @"mentions";
-            delay = @"1000";
-        }
-        
-        [sender stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"function HostAppGo() { start('%@') }", action]];
-    }
+	} else if(sender == conversationView) {
+	
+		[conversationView stringByEvaluatingJavaScriptFromString:@"function HostAppGo() { start('conversation') }"];
+		
+	} else if(sender == profileView) {
+		
+		[profileView stringByEvaluatingJavaScriptFromString:@"function HostAppGo() { start('profile') }"];
+		
+	} else {
+		
+		NSString *action = @"timeline";
+		NSString *delay = @"1";
+		
+		if (sender == mentionsView) {
+			action = @"mentions";
+			delay = @"1000";
+		}
+		
+		[sender stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"function HostAppGo() { start('%@') }", action]];
+	}
 }
 
 - (NSArray *)webView:(WebView *)sender contextMenuItemsForElement:(NSDictionary *)element defaultMenuItems:(NSArray *)defaultMenuItems
 {
-    // FIXME
-    /*
-    NSMutableArray *menuItems = [NSMutableArray arrayWithArray:defaultMenuItems];
-    
-    for (NSMenuItem*item in defaultMenuItems) {
-        if ([[item title] isEqualToString:@"Reload"]) {
-            //[item setAction:@selector(reload:)];
-            //[item setTarget:self];
-        } else {
-            [menuItems addObject:item];
-        }
-    }*/
-    
-    return defaultMenuItems;
+	// FIXME
+	/*
+	NSMutableArray *menuItems = [NSMutableArray arrayWithArray:defaultMenuItems];
+	
+	for (NSMenuItem*item in defaultMenuItems) {
+		if ([[item title] isEqualToString:@"Reload"]) {
+			//[item setAction:@selector(reload:)];
+			//[item setTarget:self];
+		} else {
+			[menuItems addObject:item];
+		}
+	}*/
+	
+	return defaultMenuItems;
 }
 
 - (void)reload:(id)sender {
-    [timelineView stringByEvaluatingJavaScriptFromString:@"bungloo_instance.getNewData();"];
-    [mentionsView stringByEvaluatingJavaScriptFromString:@"bungloo_instance.getNewData();"];
+	[timelineView stringByEvaluatingJavaScriptFromString:@"bungloo_instance.getNewData();"];
+	[mentionsView stringByEvaluatingJavaScriptFromString:@"bungloo_instance.getNewData();"];
 }
 
 - (NSString *)pluginURL
@@ -127,8 +127,8 @@
 	NSFileManager *fileManager = [NSFileManager defaultManager];
 	NSString *pathToPlugin = [@"~/Library/Application Support/Bungloo/Plugin.js" stringByExpandingTildeInPath];
 	
-    if([fileManager fileExistsAtPath:pathToPlugin])
-    {
+	if([fileManager fileExistsAtPath:pathToPlugin])
+	{
 		return [NSString stringWithFormat:@"%@", [NSURL fileURLWithPath:pathToPlugin]];
 	}
 	return nil;
