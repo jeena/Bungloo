bb5b38fb4f8b762a279e55ec95004cec61f31637
jeena <spam@jeenaparadies.net>
2013-04-14 13:51:36 +0200

    added SingleDoubleClick

---
+++ WebKit/scripts/lib/SingleDoubleClick.js
@@ -0,0 +1,30 @@
+define([
+    "jquery"
+],
+
+function(jQuery) {
+
+// Author:  Jacek Becela
+// Source:  http://gist.github.com/399624
+// License: MIT
+
+jQuery.fn.single_double_click = function(single_click_callback, double_click_callback, timeout) {
+  return this.each(function(){
+    var clicks = 0, self = this;
+    jQuery(this).click(function(event){
+      clicks++;
+      if (clicks == 1) {
+        setTimeout(function(){
+          if(clicks == 1) {
+            single_click_callback.call(self, event);
+          } else {
+            double_click_callback.call(self, event);
+          }
+          clicks = 0;
+        }, timeout || 300);
+      }
+    });
+  });
+}
+
+})
